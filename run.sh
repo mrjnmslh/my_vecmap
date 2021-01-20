03059f7ecda4b0498aae6716f1dafe2d47d2d395
mrjnmslh <44518604+mrjnmslh@users.noreply.github.com>
2021-01-20 20:59:56 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-DATA_DIR=/data/embeddings
-src=de
+DATA_DIR=/data
+src=fa
 tgt=en
 #random_list=$(python3 -c 'import random; random.seed(0); print(" ".join([random.randint(0, 1000) for _ in range(10)]))') # random seeds
 random_list=$(python3 -c "import random; random.seed(0); print(' '.join([str(random.randint(0, 1000)) for _ in range(10)]))") # random seeds
@@ -11,5 +11,7 @@ for s in ${random_list[@]}
 do
 echo $s
 #python3 map_embeddings.py --unsupervised $DATA_DIR/wiki.$src.vec $DATA_DIR/wiki.$tgt.vec output/wiki-mapped-$src-$tgt-$s.$src output/wiki-mapped-$src-$tgt-$s.$tgt --cuda --log train_log_$src-$tgt.txt -v  --validation data/dictionaries/$src-$tgt.5000-6500.txt --unsupervised_vocab 4000 --seed $s --device_id 3
-python3 map_embeddings.py --unsupervised $DATA_DIR/$src.emb.txt $DATA_DIR/$tgt.emb.txt output/wacky-mapped-$src-$tgt-$s.$src output/wacky-mapped-$src-$tgt-$s.$tgt --cuda --log output/wacky-train_log_$src-$tgt.txt -v  --validation data/dictionaries/$src-$tgt.test.txt --unsupervised_vocab 4000 --seed $s --device_id 1
+#python3 map_embeddings.py --unsupervised $DATA_DIR/$src.emb.txt $DATA_DIR/$tgt.emb.txt output/wacky-mapped-$src-$tgt-$s.$src output/wacky-mapped-$src-$tgt-$s.$tgt --cuda --log output/wacky-train_log_$src-$tgt.txt -v  --validation data/dictionaries/$src-$tgt.test.txt --unsupervised_vocab 4000 --seed $s --device_id 1
+python map_embeddings.py --unsupervised $DATA_DIR/wiki.fa.vec $DATA_DIR/wiki.en.vec output/src.mapped.txt output/trg.mapped.txt --cuda --log output/train_log.txt -v  --validation data/dictionaries/en-fa.test.txt --unsupervised_vocab 4000 --seed $s --device_id 1
+
 done
